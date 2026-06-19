3d00012ab1a3fa61e303ca1b97487546487e5e2e
saiful-aj18 <alexjamson118@gmail.com>
2026-06-20 00:25:05 +0600

    modify test.dart

--- bin/test.dart
+++ bin/test.dart
@@ -1,5 +1,26 @@
 void main() {
   print('Hello Dart!');
+
+String number = "42";
+String doublenumber = "42.22";
+int parsenumber = int.parse(number);
+double parsenumberdouble = double.parse(doublenumber);
+print(parsenumber);
+print(parsenumberdouble);
+
+double price = 99.99;
+print(price.round());
+print(parsenumber.toString());
+
+bool isfun = true;
+bool isfunDart = false;
+
+print(isfun && !isfunDart);
+
+
+
+
+
 }
 
 
